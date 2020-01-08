587122998f5a9b69831b9defa33749775447e9bb
jinsil.hwang <jinsil.kelly.hwang@gmail.com>
2020-01-08 23:26:49 +0000

    Updates

---
+++ precache-manifest.136808f46a56a048b6420203c228f212.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9f621019ac1b0c684c07036548929a4",
+    "url": "/cognito-react/index.html"
+  },
+  {
+    "revision": "86e06ddb9b3ab5aa70a6",
+    "url": "/cognito-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fed6f359db4c87aae72f",
+    "url": "/cognito-react/static/js/2.e4f03429.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cognito-react/static/js/2.e4f03429.chunk.js.LICENSE"
+  },
+  {
+    "revision": "86e06ddb9b3ab5aa70a6",
+    "url": "/cognito-react/static/js/main.f1a75056.chunk.js"
+  },
+  {
+    "revision": "5feec46a6966eeaa74df",
+    "url": "/cognito-react/static/js/runtime-main.d4f7debd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cognito-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
